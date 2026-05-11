36f715525308f54ee5b8eed9ba7941be43a10a1f
STIS-Kampala <sheekhabobaker0@gmail.com>
2026-05-12 01:41:25 +0300

    Update database.py

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -4,10 +4,10 @@ import psycopg2
 def get_conn():
     return psycopg2.connect(
         host=os.environ["PGHOST"],
-        dbname=os.environ["POSTGRES_DB"],
-        user=os.environ["POSTGRES_USER"],
-        password=os.environ["POSTGRES_PASSWORD"],
-        port=5432,
+        port=os.environ["PGPORT"],
+        dbname=os.environ["PGDATABASE"],
+        user=os.environ["PGUSER"],
+        password=os.environ["PGPASSWORD"],
         sslmode="require"
     )
 
